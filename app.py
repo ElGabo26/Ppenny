bed8913995f17f6bd60a0507824b34ce34a3c61b
gab0o06 <sheldonlopezz@gmail.com>
2023-05-30 20:51:49 -0500

    Sin DB

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, Response, session, url_for
-from db import *
+from flask import Flask, render_template, request, redirect, Response, session
+# from db import *
 from cam import *
 
 
@@ -22,11 +22,13 @@ def generate():
     user = session.get("user")
     return Response(generate_frames(user["nombre"], user["apellido"]), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route("/cartoon")
 def cartoon():
     path = proceso()
     session["image_path"] = path
     return path
+
 
 @app.route("/result")
 def result():
@@ -40,39 +42,18 @@ def registerForm():
         nombre = request.form["nombre"].lower().capitalize()
         apellido = request.form["apellido"].lower().capitalize()
 
-        conextion_DB = connectionDB()
-        cursor = conextion_DB.cursor(buffered=True)
+        user = {
+            'nombre': nombre,
+            'apellido': apellido
+        }
+        session["user"] = user
 
-        cursor.execute("SELECT id FROM Pennyproject WHERE nombre = %s AND apellido = %s", (nombre, apellido))
-        registro = cursor.fetchone()
-        user = cursor.fetchall()
+        print("Usuario creado con Nombre: ", nombre, " y Apellido", apellido)
 
-        if registro:
-            alert_message = "El nombre y apellido ya están registrados. Por favor, elige otros."
-            return render_template("register.html", alert_message=alert_message)
-        else:
-
-            cursor.execute(
-                "INSERT INTO Pennyproject(nombre,apellido, img) VALUES (%s, %s, %s)", (nombre, apellido, ""))
-            conextion_DB.commit()
-
-            inserted_id = cursor.lastrowid
-            user = {
-                'id': inserted_id,
-                'nombre': nombre,
-                'apellido': apellido
-            }
-            session["user"] = user
-
-            cursor.close()
-            conextion_DB.close()
-
-            print("Usuario creado con: Id", inserted_id,  ", Nombre", nombre, ", Apellido", apellido)
-
-            return redirect("/generate")
+        return redirect("/generate")
     else:
         return render_template("register.html")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- cam.py
+++ cam.py
@@ -1,24 +1,25 @@
 import cv2
 import os
-from db import *
+# from db import *
 import random
 import os
 
 PathPadre = os.path.dirname(os.path.abspath(__file__))
-dataPath= PathPadre+ '/data'
-exportPath=PathPadre+ '\static\procesadas'
+dataPath = PathPadre + '/data'
+exportPath = PathPadre + '\static\procesadas'
 
 
 # Función para capturar video de la webcam
 def generate_frames(nombre, apellido):
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    personName = nombre + " " +apellido
+    personName = nombre + " " + apellido
     personPath = dataPath + '/' + personName
 
     if not os.path.exists(personPath):
         print('Carpeta Creada: ', personPath)
         os.makedirs(personPath)
-    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faceClassif = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     count = 0
 
     while True:
@@ -29,22 +30,23 @@ def generate_frames(nombre, apellido):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         auxFrame = frame.copy()
         faces = faceClassif.detectMultiScale(gray, 1.3, 5)
-        
 
         for (x, y, w, h) in faces:
             if count >= 300:
                 break
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             rostro = auxFrame[y:y + h, x:x + w]
-            rostro = cv2.resize(rostro, (720, 720), interpolation=cv2.INTER_CUBIC)
+            rostro = cv2.resize(rostro, (720, 720),
+                                interpolation=cv2.INTER_CUBIC)
             cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count), rostro)
             count += 1
-        
+
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')        
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     cap.release()
+
 
 def cartoon(numero):
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -56,7 +58,8 @@ def cartoon(numero):
     k = 25
     gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gris_blur = cv2.medianBlur(gris, blur_val)
-    edges = cv2.adaptiveThreshold(gris_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, linea, blur_val)
+    edges = cv2.adaptiveThreshold(
+        gris_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, linea, blur_val)
     blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
 
     qfinal = cv2.bitwise_and(blurred, blurred, mask=edges)
@@ -71,14 +74,16 @@ def cartoon(numero):
     enhanced = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
     # Mostrar imagen original y mejorada
-    cv2.imwrite(exportPath+ "/foto_{}.jpg".format(numero), enhanced)
+    cv2.imwrite(exportPath + "/foto_{}.jpg".format(numero), enhanced)
     cap.release()
 
     return "../static/procesadas/"+"/foto_{}.jpg".format(numero)
 
+
 def aleatorio():
-    return random.randint(1,100)
+    return random.randint(1, 100)
+
 
 def proceso():
-    numero=aleatorio()
+    numero = aleatorio()
     return cartoon(numero)
